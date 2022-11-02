1ba2e02bef15bcfa6a6d2addc0e551b0f607767c
Harrison Ryan <hwryan12@gmail.com>
2022-11-02 14:25:30 -0500

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+#Strings
+friends = ["Kurt", "Johnny", "Kris", "Paulina"]
+#Integers
+ages = [30, 33, 40, 28]
+#Floats
+dollars = [10.5, 24.3, 87.9, 14.2]
+#Booleans
+loves_ice_cream = [true, true, false, true]
